30988982c754b67aaeca20d0b877db66e0205b07
Nivethan Kandiah <nivethan.kandiah@randers.dk>
2026-01-13 15:39:33 +0100

    add retry

--- dags/dag_sharepoint/dag_sharepoint.py
+++ dags/dag_sharepoint/dag_sharepoint.py
@@ -6,7 +6,7 @@ from utils.config import DEFAULT_DAG_ARGS
 from dag_sharepoint.process_sharepoint import process_sharepoint_list_items
 
 dag_args = DEFAULT_DAG_ARGS.copy()
-dag_args["retries"] = 0
+dag_args["retries"] = 1
 
 with DAG(
     dag_id="dag_sharepoint",
