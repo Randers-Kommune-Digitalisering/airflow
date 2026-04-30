7a2fe380cb468c1084708934c906d3db8e91bab2
Rune Aagaard Keena <Rune.Aagaard.Keena@randers.dk>
2026-04-30 19:42:01 +0200

    -

--- dags/dag_serviceplatformen_test/testing.py
+++ dags/dag_serviceplatformen_test/testing.py
@@ -1,20 +1,5 @@
 def test():
-    from pathlib import Path
     from utils.kombit import TempClientCert
-
-    DAG_DIR = Path(__file__).parent
-    certs_dir = DAG_DIR / "certs"
-    certs_dir.mkdir(parents=True, exist_ok=True)
-    paths = [
-        certs_dir / "ADG_PROD_Adgangsstyring_2.cer",
-        certs_dir / "YDI_PROD_Ydelsesindeks_2.cer"
-    ]
-
-    for fp in paths:
-        if fp.exists():
-            print(f"Found certificate file: {fp}")
-        else:
-            print(f"!!! Certificate file not found: {fp}")
 
     with TempClientCert() as client_cert_path:
         print(f"Client certificate .p12 file created at: {client_cert_path}")
