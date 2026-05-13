d5853b71d1fffa6c24542df0ec7ae9d23397aed7
Nivethan Kandiah <nivethan.kandiah@randers.dk>
2026-05-13 11:22:30 +0200

    replace config file with Airflow Variabel
    config will now be maintained in Airflow Variables without any code changes or deployment

--- dags/dag_jobindsats/jobindsats_config.py
+++ dags/dag_jobindsats/jobindsats_config.py
@@ -1,200 +1,200 @@
-JOBINDSATS_CONFIG = [
-    {
-        "name": "Ledighed: Faktiske antal ledige og fuldtidsledige",
-        "years_back": 5,
-        "dataset": "y25i01",
-        "period_format": "M",
-        "data_to_get": {
-        }
-    },
-    {
-        "name": "Ydelsesmodtagere med løntimer",
-        "years_back": 2,
-        "dataset": "otij01",
-        "period_format": "M",
-        "data_to_get": {
-            "_ygrp_j01": [
-                "Kontanthjælp",
-                "Jobafklaringsforløb",
-                "Forrevalidering og revalidering",
-                "Ressourceforløb"
-            ],
-            "_maalgrp": [
-                "Jobparate mv."
-            ]
-        }
-    },
-    {
-        "name": "Offentligt forsørgede",
-        "years_back": 2,
-        "dataset": "ptv_a02",
-        "period_format": "M",
-        "data_to_get": {
-            "_ygrpa02": [
-                "Førtidspension",
-                "Efterløn",
-                "Seniorpension",
-                "Jobafklaringsforløb",
-                "Tidlig pension"
-            ],
-            "_kon": [
-                "Kvinder",
-                "Mænd"
-            ]
-        }
-    },
-    {
-        "name": "Tilbud og samtaler",
-        "years_back": 2,
-        "dataset": "ptvc01",
-        "period_format": "M",
-        "data_to_get": {
-            "_ygrpc02": [
-                "A-dagpenge",
-                "Kontanthjælp",
-                "Sygedagpenge"
-            ]
-        }
-    },
-    {
-        "name": "A-Dagpenge",
-        "years_back": 5,
-        "dataset": "y01a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Sygedagpenge",
-        "years_back": 5,
-        "dataset": "y07a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Fleksjob",
-        "years_back": 5,
-        "dataset": "y08a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Ledighedsydelse",
-        "years_back": 5,
-        "dataset": "y09a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Ressourceforløb",
-        "years_back": 5,
-        "dataset": "y11a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Jobafklaringsforløb",
-        "years_back": 5,
-        "dataset": "y12a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Kontanthjælp",
-        "years_back": 3,
-        "dataset": "y60a02",
-        "id": "satser",
-        "period_format": "M",
-        "data_to_get": {
-            "_kth_sats": [
-                "Kontanthjælpssatser i alt",
-                "Forhøjet sats",
-                "Grundsats",
-                "Mindstesats omfattet af program",
-                "Mindstesats øvrige"
-            ]
-        }
-    },
-    {
-        "name": "Kontanthjælp",
-        "years_back": 3,
-        "id": "jobparat_satser",
-        "dataset": "y60a02",
-        "period_format": "M",
-        "data_to_get": {
-            "_viskat_1int": [
-                "Jobparat"
-            ],
-            "_kth_sats": [
-                "Kontanthjælpssatser i alt",
-                "Forhøjet sats",
-                "Grundsats",
-                "Mindstesats omfattet af program",
-                "Mindstesats øvrige"
-            ]
-        }
-    },
-    {
-        "name": "Barselsdagpenge",
-        "years_back": 5,
-        "dataset": "y40a02",
-        "period_format": "M",
-        "data_to_get": {}
-    },
-    {
-        "name": "Fra ydelse til job",
-        "years_back": 2,
-        "dataset": "y14d03",
-        "period_format": "QMAT",
-        "data_to_get": {
-            "_ygrpmm12": [
-                "A-dagpenge",
-                "Kontanthjælp",
-                "Sygedagpenge"
-            ],
-            "_tilbud_d03": [
-                "Privat virksomhedspraktik",
-                "Offentlig virksomhedspraktik"
-            ],
-            "_maalgrp_d03": [
-                "Jobparate mv.",
-                "Aktivitetsparate mv."
-            ]
-        }
-    },
-    {
-        "name": "Ydelsesgrupper",
-        "years_back": 4,
-        "dataset": "y30r21",
-        "period_format": "QMAT",
-        "data_to_get": {
-            "_ygrp_y30r21": [
-                "Ydelsesgrupper i alt",
-                "A-dagpenge mv.",
-                "Sygedagpenge mv.",
-                "Kontanthjælp mv."
-            ]
-        }
-    },
-    {
-        "name": "Sygedagpenge",
-        "years_back": 5,
-        "dataset": "y07a07",
-        "period_format": "M",
-        "data_to_get": {
-            "area": ["Randers", "Hele landet"]
-        }
-    },
-    {
-        "name": "Andel i beskæftigelse 3, 6, 9 og 12 mdr. efter nyledighed",
-        "years_back": 3,
-        "dataset": "y25i08",
-        "period_format": "QMAT",
-        "data_to_get": {
-            "_opdel_akt": [
-                "I alt",
-                "A-dagpenge",
-                "Kontanthjælp"
-            ]
-        }
-    }
-]
+# JOBINDSATS_CONFIG = [
+#     {
+#         "name": "Ledighed: Faktiske antal ledige og fuldtidsledige",
+#         "years_back": 5,
+#         "dataset": "y25i01",
+#         "period_format": "M",
+#         "data_to_get": {
+#         }
+#     },
+#     {
+#         "name": "Ydelsesmodtagere med løntimer",
+#         "years_back": 2,
+#         "dataset": "otij01",
+#         "period_format": "M",
+#         "data_to_get": {
+#             "_ygrp_j01": [
+#                 "Kontanthjælp",
+#                 "Jobafklaringsforløb",
+#                 "Forrevalidering og revalidering",
+#                 "Ressourceforløb"
+#             ],
+#             "_maalgrp": [
+#                 "Jobparate mv."
+#             ]
+#         }
+#     },
+#     {
+#         "name": "Offentligt forsørgede",
+#         "years_back": 2,
+#         "dataset": "ptv_a02",
+#         "period_format": "M",
+#         "data_to_get": {
+#             "_ygrpa02": [
+#                 "Førtidspension",
+#                 "Efterløn",
+#                 "Seniorpension",
+#                 "Jobafklaringsforløb",
+#                 "Tidlig pension"
+#             ],
+#             "_kon": [
+#                 "Kvinder",
+#                 "Mænd"
+#             ]
+#         }
+#     },
+#     {
+#         "name": "Tilbud og samtaler",
+#         "years_back": 2,
+#         "dataset": "ptvc01",
+#         "period_format": "M",
+#         "data_to_get": {
+#             "_ygrpc02": [
+#                 "A-dagpenge",
+#                 "Kontanthjælp",
+#                 "Sygedagpenge"
+#             ]
+#         }
+#     },
+#     {
+#         "name": "A-Dagpenge",
+#         "years_back": 5,
+#         "dataset": "y01a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Sygedagpenge",
+#         "years_back": 5,
+#         "dataset": "y07a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Fleksjob",
+#         "years_back": 5,
+#         "dataset": "y08a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Ledighedsydelse",
+#         "years_back": 5,
+#         "dataset": "y09a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Ressourceforløb",
+#         "years_back": 5,
+#         "dataset": "y11a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Jobafklaringsforløb",
+#         "years_back": 5,
+#         "dataset": "y12a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Kontanthjælp",
+#         "years_back": 3,
+#         "dataset": "y60a02",
+#         "id": "satser",
+#         "period_format": "M",
+#         "data_to_get": {
+#             "_kth_sats": [
+#                 "Kontanthjælpssatser i alt",
+#                 "Forhøjet sats",
+#                 "Grundsats",
+#                 "Mindstesats omfattet af program",
+#                 "Mindstesats øvrige"
+#             ]
+#         }
+#     },
+#     {
+#         "name": "Kontanthjælp",
+#         "years_back": 3,
+#         "id": "jobparat_satser",
+#         "dataset": "y60a02",
+#         "period_format": "M",
+#         "data_to_get": {
+#             "_viskat_1int": [
+#                 "Jobparat"
+#             ],
+#             "_kth_sats": [
+#                 "Kontanthjælpssatser i alt",
+#                 "Forhøjet sats",
+#                 "Grundsats",
+#                 "Mindstesats omfattet af program",
+#                 "Mindstesats øvrige"
+#             ]
+#         }
+#     },
+#     {
+#         "name": "Barselsdagpenge",
+#         "years_back": 5,
+#         "dataset": "y40a02",
+#         "period_format": "M",
+#         "data_to_get": {}
+#     },
+#     {
+#         "name": "Fra ydelse til job",
+#         "years_back": 2,
+#         "dataset": "y14d03",
+#         "period_format": "QMAT",
+#         "data_to_get": {
+#             "_ygrpmm12": [
+#                 "A-dagpenge",
+#                 "Kontanthjælp",
+#                 "Sygedagpenge"
+#             ],
+#             "_tilbud_d03": [
+#                 "Privat virksomhedspraktik",
+#                 "Offentlig virksomhedspraktik"
+#             ],
+#             "_maalgrp_d03": [
+#                 "Jobparate mv.",
+#                 "Aktivitetsparate mv."
+#             ]
+#         }
+#     },
+#     {
+#         "name": "Ydelsesgrupper",
+#         "years_back": 4,
+#         "dataset": "y30r21",
+#         "period_format": "QMAT",
+#         "data_to_get": {
+#             "_ygrp_y30r21": [
+#                 "Ydelsesgrupper i alt",
+#                 "A-dagpenge mv.",
+#                 "Sygedagpenge mv.",
+#                 "Kontanthjælp mv."
+#             ]
+#         }
+#     },
+#     {
+#         "name": "Sygedagpenge",
+#         "years_back": 5,
+#         "dataset": "y07a07",
+#         "period_format": "M",
+#         "data_to_get": {
+#             "area": ["Randers", "Hele landet"]
+#         }
+#     },
+#     {
+#         "name": "Andel i beskæftigelse 3, 6, 9 og 12 mdr. efter nyledighed",
+#         "years_back": 3,
+#         "dataset": "y25i08",
+#         "period_format": "QMAT",
+#         "data_to_get": {
+#             "_opdel_akt": [
+#                 "I alt",
+#                 "A-dagpenge",
+#                 "Kontanthjælp"
+#             ]
+#         }
+#     }
+# ]
--- dags/dag_jobindsats/process_jobindsats.py
+++ dags/dag_jobindsats/process_jobindsats.py
@@ -3,11 +3,30 @@ import logging
 
 from airflow.providers.http.hooks.http import HttpHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-
+from typing import Any
 from dag_jobindsats.jobindsats_data import fetch_and_store_table_updates, get_data
-from dag_jobindsats.jobindsats_config import JOBINDSATS_CONFIG
+from airflow.models import Variable
+from airflow.exceptions import AirflowException
 
 logger = logging.getLogger(__name__)
+
+
+def _load_jobindsats_config() -> list[dict[str, Any]]:
+    variable_value: Any = Variable.get("jobindsats_config", default_var=None, deserialize_json=True,)
+
+    if variable_value is None:
+        raise AirflowException("Airflow Variable 'jobindsats_config' is missing or empty")
+
+    config_list: Any
+    if isinstance(variable_value, dict):
+        config_list = variable_value.get("jobindsats_config")
+    else:
+        config_list = variable_value
+
+    if not isinstance(config_list, list):
+        raise AirflowException("Airflow Variable 'jobindsats_config' must be a JSON list, ")
+
+    return config_list
 
 
 def process_jobindsats() -> None:
@@ -16,7 +35,7 @@ def process_jobindsats() -> None:
     jobindsats_db_engine = jobindsats_db_hook.get_sqlalchemy_engine()
 
     fetch_and_store_table_updates(http_hook=jobindsats_http_hook, db_engine=jobindsats_db_engine)
-    for job in JOBINDSATS_CONFIG:
+    for job in _load_jobindsats_config():
         kwargs = {
             "http_hook": jobindsats_http_hook,
             "db_engine": jobindsats_db_engine,
