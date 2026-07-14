5466ebfb745c64a1dd5be2966a17e6a44624b28f
Søren Thomassen <st@randers.dk>
2026-07-14 14:40:53 +0200

    Add email construction + send results to email

--- dags/dag_fritidsjobs_webscraper/process_fritidsjobs_webscraper.py
+++ dags/dag_fritidsjobs_webscraper/process_fritidsjobs_webscraper.py
@@ -1,12 +1,13 @@
 import logging
 from typing import Any
 from dag_fritidsjobs_webscraper.scapy_client import scrape_sites
+from dag_fritidsjobs_webscraper.email_construction import construct_email
 
 from airflow.models import Variable
 # from airflow.hooks.base import BaseHook
 # from airflow.exceptions import AirflowFailException
 # from dag_fritidsjobs_webscraper.fritidsjobs_webscraper_data import placeholder_function
-# from rkdigi.email_handling import EmailSender
+from rkdigi.email_handling import EmailSender
 # from rkdigi.database_manager import DatabaseManager
 
 import json
@@ -28,6 +29,13 @@ def process_fritidsjobs_webscraper() -> list[dict[str, Any]]:
     if not isinstance(sites_config, list):
         raise TypeError("runtime_config['sites'] must be a list of dicts")
 
+    sender = runtime_config["sender_email"]
+    recipients = runtime_config["recipient_emails"]
+    smtp_server = runtime_config["smtp_server"]
+
+    if not all(isinstance(email, str) for email in [sender] + recipients):
+        raise TypeError("runtime_config['sender_email'] and runtime_config['recipient_emails'] must be strings")
+
     site_configs = sites_config
 
     if not all(isinstance(site_config, dict) for site_config in site_configs):
@@ -35,7 +43,21 @@ def process_fritidsjobs_webscraper() -> list[dict[str, Any]]:
 
     scraped_sites = scrape_sites(site_configs)
     logger.info("Finished scraping fritidsjobs_webscraper data: %s", json.dumps(scraped_sites, ensure_ascii=False))
-    return scraped_sites
+
+    subject, email_body = construct_email(scraped_sites)
+    logger.info("Constructed email subject: %s", subject)
+    logger.info("Constructed email body: %s", email_body)
+
+    email_sender = EmailSender(smtp_server=smtp_server)
+    email_sender.send_email(
+        sender=sender,
+        recipients=recipients,
+        subject=subject,
+        body=email_body,
+        attachments=[],
+    )
+
+    # return scraped_sites
 
     # placeholder_function()
 
