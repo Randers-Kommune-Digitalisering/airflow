ca7e3e601ca76b28a7ead80f28f610e3f56cf3da
Nivethan Kandiah <nivethan.kandiah@randers.dk>
2026-09-02 12:22:37 +0200

    added full flow

--- dags/dag_sd_control_and_error_list_review/process_sd_control_and_error_list_review.py
+++ dags/dag_sd_control_and_error_list_review/process_sd_control_and_error_list_review.py
@@ -1,15 +1,36 @@
 import logging
 
-# from airflow.exceptions import AirflowFailException
-
-from dag_sd_control_and_error_list_review.sd_control_and_error_list_review_data import placeholder_function
-
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
+from dag_sd_control_and_error_list_review.sd_control_and_error_list_review_data import run_sd_control_and_error_list_review_job
+from airflow.exceptions import AirflowFailException
 logger = logging.getLogger(__name__)
 
 
 def process_sd_control_and_error_list_review() -> None:
     """
-    Placeholder function for processing the sd_control_and_error_list_review data.
+    Review SD control and error messages for the configured departments.
     """
     logger.info("Starting to process sd_control_and_error_list_review data...")
-    placeholder_function()
+    sd_personaleweb = BaseHook.get_connection("sd_fleksjobrefusion_personaleweb")
+    host = sd_personaleweb.host
+    username = sd_personaleweb.login
+    password = sd_personaleweb.password
+    if not username or not password or not host:
+        raise AirflowFailException(
+            "Connection 'sd_fleksjobrefusion_personaleweb' is missing host, username, or password"
+        )
+
+    sd_control_error_list_config = Variable.get("sd_control_error_list_config", deserialize_json=True)
+    department_codes = sd_control_error_list_config["department_codes"]
+    allowed_codes = sd_control_error_list_config["allowed_codes"]
+
+    logger.info(f"Department codes: {department_codes}, Allowed codes: {allowed_codes}")
+
+    run_sd_control_and_error_list_review_job(
+        username=username,
+        password=password,
+        department_codes=department_codes,
+        allowed_codes=allowed_codes,
+        sd_url=host,
+    )
